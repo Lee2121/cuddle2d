63c27f20f3951a24ed475574b7e4bf88437fef57
Lee2121 <lcudney@yahoo.com>
2025-12-28 21:37:39 -0500

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+require "lib.cuddle2d.input.inputdefs"
+require "lib.cuddle2d.input.inputdevicemanager"
+require "lib.cuddle2d.input.inputactions"
+require "lib.cuddle2d.input.inputmods"
+require "lib.cuddle2d.input.playerinputmanager"
+
+require "lib.cuddle2d.players.playerinstance"
+require "lib.cuddle2d.players.playermanager"
